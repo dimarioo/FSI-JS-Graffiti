69431b4e289ea3d03235a794d4ba6fa1e718f078
Dimario Oliver <dimariooliver@Dimarios-MacBook.local>
2022-11-14 13:43:31 -0500

    Rotated Images

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = 'left'
+let header1 = document.querySelectorAll('.dog-name')
+for(let i = 0; i < header1.length; i++){
+	header1[i].style.textAlign = 'left'
+}
+let footertext = document.querySelector('.footer')
+footertext.style.color = 'red'
+
+let dogImages = document.querySelectorAll('.dog-image')
+
+for(let i = 0; i < dogImages.length; i++){
+	dogImages[i].style.transform = 'rotate(180deg'
+}
+
+
 
 // Select some elements...
